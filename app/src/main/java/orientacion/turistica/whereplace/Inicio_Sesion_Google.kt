651c509d94dba2f91f606c0ce94c11e4fead3721
Jaime <jaime.valdez@potros.itson.edu.mx>
2023-03-27 17:17:01 -0700

    Cuarto commit se agrego Inicio_Secion_Google

---
+++ app/src/main/java/orientacion/turistica/whereplace/Inicio_Sesion_Google.kt
@@ -0,0 +1,11 @@
+package orientacion.turistica.whereplace
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Inicio_Sesion_Google : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_inicio_sesion_google)
+    }
+}
