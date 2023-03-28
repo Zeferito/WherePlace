4f0956b499ba97506b6cb70c7287558c208c5f4e
ejim7 <enrique.jimenez181605@potros.itson.edu.mx>
2023-03-27 22:04:16 -0600

    round button

---
+++ app/src/main/java/orientacion/turistica/whereplace/Perfil_Usuario.kt
@@ -0,0 +1,11 @@
+package orientacion.turistica.whereplace
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Perfil_Usuario : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_perfil_usuario)
+    }
+}
