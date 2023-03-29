08c7dbe167b6d0d72fc5598a129656567e543422
Jaime <jaime.valdez@potros.itson.edu.mx>
2023-03-29 16:11:40 -0700

    se realizo la logica para desplazarce entre pantallas de registrro usuario, se hicieron cambios en las pantallas activity_favoritos y activity_perfil_usuario debido a que el boton de regreso tiene un caracte qeu daba error de compilacion

--- app/src/main/java/orientacion/turistica/whereplace/Registro_Usuario.kt
+++ app/src/main/java/orientacion/turistica/whereplace/Registro_Usuario.kt
@@ -1,11 +1,27 @@
 package orientacion.turistica.whereplace
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 
 class Registro_Usuario : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registro_usuario)
+
+        val btn_crear: Button = findViewById(R.id.crear)
+        val btn_regreso: ImageView = findViewById(R.id.regreso)
+
+        btn_crear.setOnClickListener{
+            val intent: Intent = Intent(this, Pantalla_Inicio::class.java)
+            startActivity(intent)
+        }
+        btn_regreso.setOnClickListener{
+            val intent: Intent = Intent(this, Pantalla_Inicio::class.java)
+            startActivity(intent)
+        }
+
     }
 }
