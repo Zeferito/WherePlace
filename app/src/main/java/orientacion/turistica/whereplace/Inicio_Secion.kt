2279c5355becccb86c58e6181ed7a2046cc9d66d
Jaime <jaime.valdez@potros.itson.edu.mx>
2023-03-29 00:44:55 -0700

    Se maqueto la pantalla de inicio de secion, se realizo movimiento entre pantallas en relacion a la pantalla de inicio y se realiozaron modificaciones a la pantalla de inicio

--- app/src/main/java/orientacion/turistica/whereplace/Inicio_Secion.kt
+++ app/src/main/java/orientacion/turistica/whereplace/Inicio_Secion.kt
@@ -1,11 +1,30 @@
 package orientacion.turistica.whereplace
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
 
 class Inicio_Secion : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_inicio_secion)
+
+        val btn_iniciar: Button = findViewById(R.id.iniciar_sesion_2)
+        val btn_regreso: ImageView = findViewById(R.id.regreso)
+        val text_usuario: EditText = findViewById(R.id.usuario)
+        val text_contra: EditText = findViewById(R.id.contra)
+
+        btn_iniciar.setOnClickListener{
+            val intent: Intent = Intent(this, Pantalla_Principal::class.java)
+            startActivity(intent)
+        }
+        btn_regreso.setOnClickListener{
+            val intent: Intent = Intent(this, Pantalla_Inicio::class.java)
+            startActivity(intent)
+        }
+
     }
 }
