e042b180fd3e9f393f1b69dc3cc000d5bda53ed9
Jaime <jaime.valdez@potros.itson.edu.mx>
2023-04-05 20:52:09 -0700

    se termino el maquetado y desplasamiento de la pantalla inicio_sesion_google

--- app/src/main/java/orientacion/turistica/whereplace/Inicio_Sesion_Google.kt
+++ app/src/main/java/orientacion/turistica/whereplace/Inicio_Sesion_Google.kt
@@ -1,11 +1,29 @@
 package orientacion.turistica.whereplace
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
 
 class Inicio_Sesion_Google : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_inicio_sesion_google)
+
+        val btn_regreso: ImageView = findViewById(R.id.regreso)
+        val text_usuario: EditText = findViewById(R.id.usuario)
+        val btn_siguiente: Button = findViewById(R.id.siguiente)
+
+        btn_regreso.setOnClickListener{
+            val intent: Intent = Intent(this, Pantalla_Inicio::class.java)
+            startActivity(intent)
+        }
+        btn_siguiente.setOnClickListener{
+            val intent: Intent = Intent(this, Pantalla_Principal::class.java)
+            startActivity(intent)
+        }
+
     }
 }
