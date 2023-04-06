375bfffaa6ae689c1de57793fe7e3291d7b818c7
Jaime <jaime.valdez@potros.itson.edu.mx>
2023-04-05 20:57:41 -0700

    se agergaron las pantallas agendar_viaje y futuros_viajes

---
+++ app/src/main/java/orientacion/turistica/whereplace/Agendar_viaje.kt
@@ -0,0 +1,11 @@
+package orientacion.turistica.whereplace
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Agendar_viaje : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_agendar_viaje)
+    }
+}
---
+++ app/src/main/java/orientacion/turistica/whereplace/Futuros_viajes.kt
@@ -0,0 +1,11 @@
+package orientacion.turistica.whereplace
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Futuros_viajes : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_futuros_viajes)
+    }
+}
