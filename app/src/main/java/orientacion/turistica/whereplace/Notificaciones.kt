2b06e1fb4006c8166e6a8596994b3a1a071ac5b2
ejim7 <enrique.jimenez181605@potros.itson.edu.mx>
2023-04-14 00:17:24 -0600

    notificaciones screen done
    j

---
+++ app/src/main/java/orientacion/turistica/whereplace/Notificaciones.kt
@@ -0,0 +1,11 @@
+package orientacion.turistica.whereplace
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Notificaciones : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_notificaciones)
+    }
+}
