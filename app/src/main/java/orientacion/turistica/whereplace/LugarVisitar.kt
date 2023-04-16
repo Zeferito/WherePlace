0dd4fd6051eb09e593f23103bce036cf0e9d31fa
ejim7 <enrique.jimenez181605@potros.itson.edu.mx>
2023-04-15 23:14:47 -0600

    galeria done

---
+++ app/src/main/java/orientacion/turistica/whereplace/LugarVisitar.kt
@@ -0,0 +1,11 @@
+package orientacion.turistica.whereplace
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LugarVisitar : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lugar_visitar)
+    }
+}
