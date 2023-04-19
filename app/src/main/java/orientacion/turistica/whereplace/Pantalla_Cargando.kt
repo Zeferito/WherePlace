b1926a81947a3fdc4564da537eebca8f48a46e87
KevinIbarraPaez <98064423+KevinIbarraPaez@users.noreply.github.com>
2023-04-18 19:34:12 -0700

    Se agrego la pantalla_cargando

---
+++ app/src/main/java/orientacion/turistica/whereplace/Pantalla_Cargando.kt
@@ -0,0 +1,27 @@
+package orientacion.turistica.whereplace
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.CountDownTimer
+class Pantalla_Cargando : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pantalla_cargando)
+
+       // startTimer()
+    //}
+       // fun startTimer(){
+          //  object: CountDownTimer(3000, 1000){
+             //   override fun onTick(p0: Long) {
+
+               // }
+
+              //  override fun onFinish() {
+                //    val intent = Intent(applicationContext, Pantalla_Huellas::class.java).apply{}
+                //    startActivity(intent)
+               // }
+           // }.start()
+
+    }
+}
