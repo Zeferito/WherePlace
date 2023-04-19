4470a429e3084a908060b8acd62bb587a97b8dbe
ejim7 <enrique.jimenez181605@potros.itson.edu.mx>
2023-04-18 23:31:01 -0600

    blog added

---
+++ app/src/main/java/orientacion/turistica/whereplace/Blog.kt
@@ -0,0 +1,11 @@
+package orientacion.turistica.whereplace
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Blog : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_blog)
+    }
+}
