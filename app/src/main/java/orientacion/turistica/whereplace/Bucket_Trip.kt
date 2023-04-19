ea9b936489eeb2ce4aabfb95f358dfebfd57f3e4
KevinIbarraPaez <98064423+KevinIbarraPaez@users.noreply.github.com>
2023-04-18 21:22:19 -0700

    Se agrego bucket_trip

---
+++ app/src/main/java/orientacion/turistica/whereplace/Bucket_Trip.kt
@@ -0,0 +1,20 @@
+package orientacion.turistica.whereplace
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class Bucket_Trip : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bucket_trip)
+
+
+        val btnvolverinicio : Button = findViewById(R.id.btngohome)
+        btnvolverinicio.setOnClickListener{
+            val  intent= Intent(this,Pantalla_Principal :: class.java )
+            startActivity(intent)
+        }
+    }
+}
