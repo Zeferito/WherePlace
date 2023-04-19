fd2abfca976eb1d69d7ad60c6ece17a67d2a4530
KevinIbarraPaez <98064423+KevinIbarraPaez@users.noreply.github.com>
2023-04-18 19:56:32 -0700

    Se agrego logica de pantalla_cargando y se agrego pantalla_huellas

--- app/src/main/java/orientacion/turistica/whereplace/Pantalla_Cargando.kt
+++ app/src/main/java/orientacion/turistica/whereplace/Pantalla_Cargando.kt
@@ -9,19 +9,19 @@ class Pantalla_Cargando : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pantalla_cargando)
 
-       // startTimer()
-    //}
-       // fun startTimer(){
-          //  object: CountDownTimer(3000, 1000){
-             //   override fun onTick(p0: Long) {
+        startTimer()
+    }
+        fun startTimer(){
+            object: CountDownTimer(3000, 1000){
+               override fun onTick(p0: Long) {
 
-               // }
+                }
 
-              //  override fun onFinish() {
-                //    val intent = Intent(applicationContext, Pantalla_Huellas::class.java).apply{}
-                //    startActivity(intent)
-               // }
-           // }.start()
+               override fun onFinish() {
+                    val intent = Intent(applicationContext, Pantalla_Huellas::class.java).apply{}
+                  startActivity(intent)
+                }
+            }.start()
 
     }
 }
