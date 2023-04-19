a9a787232792941ad52d9f372424218fc0b43972
KevinIbarraPaez <98064423+KevinIbarraPaez@users.noreply.github.com>
2023-04-18 20:44:53 -0700

    se rectifico logica pantalla_huellas

--- app/src/main/java/orientacion/turistica/whereplace/Pantalla_Huellas.kt
+++ app/src/main/java/orientacion/turistica/whereplace/Pantalla_Huellas.kt
@@ -10,6 +10,17 @@ class Pantalla_Huellas : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pantalla_huellas)
 
+        val btn : Button= findViewById(R.id.btnfin1)
+        btn.setOnClickListener{
+            val  intent= Intent(this,Fin_Dinamica :: class.java )
+            startActivity(intent)
+        }
+
+        val btn2 : Button= findViewById(R.id.btnfin2)
+        btn.setOnClickListener{
+            val  intent= Intent(this,Fin_Dinamica :: class.java )
+            startActivity(intent)
+        }
 
 
         }
